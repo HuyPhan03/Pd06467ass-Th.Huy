6c0cab9bc415a15985b60c0fa0ab58ce5e53489f
THanh Huy <phanthanhhuy210820003@gmail.com>
2024-01-22 21:04:14 +0700

    test git push

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import React, {Component} from 'react';
+import {Text, StyleSheet, View} from 'react-native';
 
 export default class HomeScreen extends Component {
   render() {
@@ -7,8 +7,8 @@ export default class HomeScreen extends Component {
       <View>
         <Text> Hello </Text>
       </View>
-    )
+    );
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
